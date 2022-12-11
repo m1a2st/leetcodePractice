6da567932dee2195f074dea7a757d8e1e707c4a1
m1a2st <s7133700@gmail.com>
2022-12-11 20:10:31 +0800

    練習一些字串方法，寫一些簡單測試

---
+++ src/leecode/practice/CharCantRepeat.java
@@ -0,0 +1,30 @@
+package leecode.practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author
+ * @Date
+ * @Version
+ * @Description
+ */
+public class CharCantRepeat {
+    public static void main(String[] args) {
+        String str = "wqazsx";
+        System.out.println(compareChar(str));
+    }
+
+    public static boolean compareChar(String str) {
+        int checker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i) - 'a';
+            System.out.println(val);
+            if((checker & (1 << val)) > 0){
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+}
--- src/leecode/test.java
+++ src/leecode/test.java
@@ -13,15 +13,15 @@ import java.io.InputStream;
  */
 public class test {
     public static void main(String[] args) throws IOException {
-        Process process = Runtime.getRuntime().exec("top");
-        InputStream inputStream = process.getInputStream();
-        BufferedInputStream bis = new BufferedInputStream(inputStream);
-        byte[] buffer = new byte[1024];
-        int bytesRead = 0;
-        while ((bytesRead = bis.read(buffer)) != -1) {
-            String topString = new String(buffer, 0, bytesRead);
-            System.out.println(topString);
-        }
+//        Process process = Runtime.getRuntime().exec("top");
+//        InputStream inputStream = process.getInputStream();
+//        BufferedInputStream bis = new BufferedInputStream(inputStream);
+//        byte[] buffer = new byte[1024];
+//        int bytesRead = 0;
+//        while ((bytesRead = bis.read(buffer)) != -1) {
+//            String topString = new String(buffer, 0, bytesRead);
+//            System.out.println(topString);
+//        }
     }
 }
 
