7b12c65aa24e6a61950fcf61c4c6f17d4cefc54b
m1a2st <s7133700@gmail.com>
2023-07-25 21:21:24 +0800

    練習到 backtracking

--- src/leecode/medium/No78.java
+++ src/leecode/medium/No78.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class No78 {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3};
+        int[] a = {1, 2, 3};
         subsets(a).forEach(System.out::println);
     }
 
@@ -22,8 +22,36 @@ public class No78 {
         for (int i = start; i < nums.length; i++) {
             list.add(nums[i]);
             result.add(new ArrayList<>(list));
-            recursive(nums,result,list,i+1);
-            list.remove(list.size() -1);
+            recursive(nums, result, list, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    class Solution {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        public List<List<Integer>> subsets(int[] nums) {
+            List<Integer> track = new ArrayList<>();
+            boolean[] used = new boolean[nums.length];
+            recursive(nums, track, used);
+            return lists;
+        }
+
+        private void recursive(int[] nums, List<Integer> track, boolean[] used) {
+            if (track.size() == used.length) {
+                lists.add(new ArrayList<>(track));
+                return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if(used[i]){
+                    continue;
+                }
+                track.add(nums[i]);
+                used[i] = true;
+                recursive(nums, track, used);
+                track.remove(track.size() - 1);
+                used[i] = false;
+            }
         }
     }
 }
