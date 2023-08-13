3466eea70de27ef112fb23f6dc1f873336552d66
m1a2st <s7133700@gmail.com>
2023-08-13 17:32:25 +0800

    練習到 DP

--- src/leecode/medium/No198.java
+++ src/leecode/medium/No198.java
@@ -16,4 +16,19 @@ public class No198 {
             return rob[n - 1];
         }
     }
+
+    static class SolutionNew {
+        public int rob(int[] nums) {
+            int len = nums.length;
+            if (len == 0) return 0;
+            if (len == 1) return nums[0];
+            int[] rob = new int[len];
+            rob[0] = nums[0];
+            rob[1] = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < len; i++) {
+                rob[i] = Math.max(rob[i - 1], rob[i - 2] + nums[i]);
+            }
+            return rob[len - 1];
+        }
+    }
 }
--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -4,11 +4,28 @@ import kotlin.ranges.IntRange;
 import leecode.medium.No875;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestClass {
+
+    @Test
+    public void test() {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add("a" + i);
+        }
+
+        for (String s : list) {
+            if (s.equals("a3")) {
+                list.remove(s);
+            }
+        }
+    }
 
 //    @Test
 //    public void no875_1() {
@@ -47,12 +64,12 @@ public class TestClass {
 
     @Test
     public void no875_5() {
-        IntStream.range(0,10)
+        IntStream.range(0, 10)
                 .mapToObj(Cat::new)
                 .forEach(System.out::println);
     }
 
-    class Cat{
+    class Cat {
         private int age;
 
         public Cat() {
