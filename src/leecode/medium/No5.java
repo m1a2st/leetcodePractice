aa752f1fd3657d0b14d9b6b75f5cd182b80c6bf5
m1a2st <s7133700@gmail.com>
2023-08-14 23:31:43 +0800

    練習到 DP

--- src/leecode/medium/No5.java
+++ src/leecode/medium/No5.java
@@ -1,5 +1,7 @@
 package leecode.medium;
 
+import java.util.Objects;
+
 /**
  * @Author m1a2st
  * @Date 2023/3/22
@@ -8,25 +10,40 @@ package leecode.medium;
 public class No5 {
 
     class Solution {
-//        public String longestPalindrome(String s) {
-//            int point = 1;
-//            String[] arr = s.split("");
-//            for (int i = 0; i < arr.length; i++) {
-//            }
-//
-//        }
-
-        private boolean isPalindrome(String s) {
-            String[] arr = s.split("");
-            int left = 0, right = arr.length - 1;
-            while(left < right){
-                if(arr[left] != arr[right]){
-                    return false;
-                }
-                left++;
-                right--;
+        public String longestPalindrome(String s) {
+            int len = s.length();
+            // 空串和只有一個字符的字符串都是回文串
+            if (len < 2) {
+                return s;
             }
-            return true;
+            String res = s.substring(0, 1);
+            boolean dp[][] = new boolean[len][len];
+            for (int i = 0; i < len; i++) {
+                dp[i][i] = true;
+            }
+            char[] chars = s.toCharArray();
+            // 枚舉最長回文串的長度
+            for (int k = 2; k <= len; k++) {
+                for (int i = 0; i < len; i++) {
+                    int j = k + i - 1;
+                    if (j >= len) {
+                        break;
+                    }
+                    if (chars[i] != chars[j]) {
+                        dp[i][j] = false;
+                        continue;
+                    }
+                    if (j - i <= 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                    if (dp[i][j] && res.length() < j - i + 1) {
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+            return res;
         }
     }
 }
