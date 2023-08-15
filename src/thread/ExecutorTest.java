915bc09d5af4e7e288f8947dd3a7411f9e89f9ad
m1a2st <s7133700@gmail.com>
2023-08-15 23:20:17 +0800

    練習到 位元操作符

---
+++ src/thread/ExecutorTest.java
@@ -0,0 +1,19 @@
+package thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Author m1a2st
+ * @Date 2023/8/15
+ * @Version v1.0
+ */
+public class ExecutorTest {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+        }
+    }
+}
