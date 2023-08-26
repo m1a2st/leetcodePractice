da8d79f7a5e86b5c7adcea2218daca51219bc3f5
m1a2st <s7133700@gmail.com>
2023-08-26 14:38:55 +0800

    練習到 Dynamic Programming

---
+++ src/leecode/easy/No88.java
@@ -0,0 +1,14 @@
+package leecode.easy;
+
+import java.util.Arrays;
+
+public class No88 {
+
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            if (n >= 0)
+                System.arraycopy(nums2, 0, nums1, m, n);
+            Arrays.sort(nums1);
+        }
+    }
+}
