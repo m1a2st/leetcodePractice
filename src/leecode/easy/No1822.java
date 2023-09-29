238ebd0257e2c7c4c2c9a37f4a14285b97871d39
m1a2st <s7133700@gmail.com>
2023-09-29 20:03:10 +0800

    練習到 array

---
+++ src/leecode/easy/No1822.java
@@ -0,0 +1,18 @@
+package leecode.easy;
+
+public class No1822 {
+
+    class Solution {
+        public int arraySign(int[] nums) {
+            boolean isPositive = true;
+            for (int num : nums) {
+                if (num < 0) {
+                    isPositive = !isPositive;
+                } else if (num == 0) {
+                    return 0;
+                }
+            }
+            return isPositive ? 1 : -1;
+        }
+    }
+}
