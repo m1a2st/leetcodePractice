067d4580d3cb7c83f122d1a0f3797e4be3a0eb04
m1a2st <s7133700@gmail.com>
2023-10-16 19:59:56 +0800

    練習到 sliding window

--- src/leecode/medium/No904.java
+++ src/leecode/medium/No904.java
@@ -1,6 +1,7 @@
 package leecode.medium;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,6 +28,27 @@ public class No904 {
                     bucket.put(leftFruit, bucket.get(leftFruit) - 1);
                     if (bucket.get(leftFruit) == 0) {
                         bucket.remove(leftFruit);
+                    }
+                    left++;
+                }
+                max = Math.max(max, right - left + 1);
+            }
+            return max;
+        }
+    }
+
+    class SolutionNew {
+        public int totalFruit(int[] fruits) {
+            int max = 0;
+            int len = fruits.length;
+            Map<Integer, Integer> bucket = new HashMap<>();
+            for (int right = 0, left = 0; right < len; right++) {
+                bucket.put(fruits[right], bucket.getOrDefault(fruits[right], 0));
+                while (bucket.size() > 2 && left < right) {
+                    int fruit = fruits[left];
+                    bucket.put(fruit, bucket.get(fruit) - 1);
+                    if (bucket.get(fruit) == 0) {
+                        bucket.remove(fruit);
                     }
                     left++;
                 }
--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class TestClass {
 
     @Test
-    public void test1(){
+    public void test1() {
         System.out.println(Test.class.getSimpleName());
     }
 
@@ -19,56 +19,6 @@ public class TestClass {
         System.out.println(System.getProperty("os.name"));
         System.out.println(System.getProperty("java.version"));
     }
-
-//    @Test
-//    public void test() {
-//        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
-//
-//        for (int i = 0; i < 10; i++) {
-//            list.add("a" + i);
-//        }
-//
-//        for (String s : list) {
-//            if (s.equals("a3")) {
-//                list.remove(s);
-//            }
-//        }
-//    }
-
-//    @Test
-//    public void no875_1() {
-//        int[] piles = {3, 6, 7, 11};
-//        int h = 8;
-//        No875.Solution solution = new No875.Solution();
-//        int i = solution.minEatingSpeed(piles, h);
-//        assertEquals(4, i);
-//    }
-//
-//    @Test
-//    public void no875_2() {
-//        int[] piles = {30, 11, 23, 4, 20};
-//        int h = 5;
-//        No875.Solution solution = new No875.Solution();
-//        int i = solution.minEatingSpeed(piles, h);
-//        assertEquals(30, i);
-//    }
-//
-//    @Test
-//    public void no875_3() {
-//        int[] piles = {30, 11, 23, 4, 20};
-//        int h = 6;
-//        No875.Solution solution = new No875.Solution();
-//        int i = solution.minEatingSpeed(piles, h);
-//        assertEquals(23, i);
-//    }
-//    @Test
-//    public void no875_4() {
-//        int[] piles = {805306368,805306368,805306368};
-//        int h = 1000000000;
-//        No875.Solution solution = new No875.Solution();
-//        int i = solution.minEatingSpeed(piles, h);
-//        assertEquals(3, i);
-//    }
 
     @Test
     public void test3() {
@@ -83,8 +33,6 @@ public class TestClass {
                 .collect(Collectors.toMap(Map.Entry::getKey,
                         entry -> entry.getValue().stream()
                                 .map(Cat::toNewCat).collect(Collectors.toList())));
-
-
     }
 
 
