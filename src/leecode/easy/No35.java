66edef71a5b04748416f870cd78867300426aed5
m1a2st <s7133700@gmail.com>
2023-11-03 20:34:25 +0800

    練習到 binary search

--- src/leecode/easy/No35.java
+++ src/leecode/easy/No35.java
@@ -24,4 +24,21 @@ public class No35 {
             return l;
         }
     }
+
+    class SolutionNew {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0, right = nums.length - 1;
+            while (left <= right) {
+                int mid = (right - left) / 2 + left;
+                if (target == nums[mid]) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
 }
