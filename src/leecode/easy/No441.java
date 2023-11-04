e050ddb0089c4ab826bb108d3aba6a0c5215a35a
m1a2st <s7133700@gmail.com>
2023-11-04 09:41:39 +0800

    練習到 binary search

---
+++ src/leecode/easy/No441.java
@@ -0,0 +1,29 @@
+package leecode.easy;
+
+import org.junit.Test;
+
+public class No441 {
+
+    @Test
+    public void test(){
+        System.out.println(Math.sqrt(4));
+    }
+
+    class Solution {
+        public int arrangeCoins(int n) {
+            int step = 0;
+            while (n > step) {
+                step++;
+                n -= step;
+            }
+            return step;
+        }
+    }
+
+    class SolutionNew {
+        public int arrangeCoins(long n) {
+            return (int) (-1 + Math.sqrt(8 * n + 1)) / 2;
+        }
+    }
+
+}
