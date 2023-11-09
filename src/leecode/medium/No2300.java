036d380d09bd75c80dac38cfb061d0fbc7dc69ef
m1a2st <s7133700@gmail.com>
2023-11-09 21:43:38 +0800

    練習到 binary search

--- src/leecode/medium/No2300.java
+++ src/leecode/medium/No2300.java
@@ -23,13 +23,39 @@ public class No2300 {
             int r = potions.length;
             while (l < r) {
                 int mid = (r - l) / 2 + l;
-                if((long) potions[mid] * spell >= success){
+                if ((long) potions[mid] * spell >= success) {
                     r = mid;
-                }else{
+                } else {
                     l = mid + 1;
                 }
             }
             return l;
+        }
+    }
+
+    class SolutionNew {
+        public int[] successfulPairs(int[] spells, int[] potions, long success) {
+            int sLen = spells.length;
+            int pLen = potions.length;
+            int[] ans = new int[sLen];
+            Arrays.sort(potions);
+            for (int i = 0; i < sLen; i++) {
+                ans[i] = pLen - checkSuccessCount(spells[i], pLen, potions, success);
+            }
+            return ans;
+        }
+
+        private int checkSuccessCount(int spell, int right, int[] potions, long success) {
+            int left = 0;
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                if ((long) potions[mid] * spell >= success) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
         }
     }
 }
