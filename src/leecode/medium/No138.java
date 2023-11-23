02042a8fb6cee8a7351fff9eae2ebda4526a185d
m1a2st <s7133700@gmail.com>
2023-11-23 20:25:26 +0800

    feat(Linked-List): practice
    460 so hard, 138

--- src/leecode/medium/No138.java
+++ src/leecode/medium/No138.java
@@ -33,4 +33,19 @@ public class No138 {
             return map.get(head);
         }
     }
+
+    public class SolutionNew {
+        Map<Node, Node> map = new HashMap<>();
+
+        public Node copyRandomList(Node head) {
+            if (head == null) return null;
+            if (!map.containsKey(head)) {
+                Node newHead = new Node(head.val);
+                map.put(head, newHead);
+                newHead.next = copyRandomList(head.next);
+                newHead.random = copyRandomList(head.random);
+            }
+            return map.get(head);
+        }
+    }
 }
