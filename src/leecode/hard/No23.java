a88fe87d50d79ca8291eac459f440fd2860a3595
m1a2st <s7133700@gmail.com>
2023-12-05 20:30:34 +0800

    feat(Linked-List): practice
    23, 725 done

--- src/leecode/hard/No23.java
+++ src/leecode/hard/No23.java
@@ -28,4 +28,47 @@ public class No23 {
         }
         return dummy.next;
     }
+
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            if (lists == null || lists.length == 0) {
+                return null;
+            }
+
+            while (lists.length > 1) {
+                ListNode[] newLists = new ListNode[(lists.length + 1) / 2];
+                int j = 0;
+                for (int i = 0; i < lists.length; i += 2) {
+                    if (i + 1 < lists.length) {
+                        newLists[j++] = mergeLists(lists[i], lists[i + 1]);
+                    } else {
+                        //odd number of lists
+                        newLists[j++] = lists[i];
+                    }
+                }
+                lists = newLists;
+            }
+
+            return lists[0];
+        }
+
+
+        public ListNode mergeLists(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode current = dummy;
+            while (l2 != null && l1 != null) {
+                if (l1.val < l2.val) {
+                    current.next = l1;
+                    l1 = l1.next;
+                } else {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                current = current.next;
+            }
+            current.next = (l2 == null) ? l1 : l2;
+
+            return dummy.next;
+        }
+    }
 }
