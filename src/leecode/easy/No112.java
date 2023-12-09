77f23c179a325de6d7b8bf9f4652cc89897371db
m1a2st <s7133700@gmail.com>
2023-12-09 09:43:49 +0800

    feat(Tree): practice
    112 617

---
+++ src/leecode/easy/No112.java
@@ -0,0 +1,39 @@
+package leecode.easy;
+
+public class No112 {
+
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return false;
+            }
+            if (root.val == targetSum && root.left == null && root.right == null) {
+                return true;
+            }
+            int diff = targetSum - root.val;
+            return hasPathSum(root.right, diff) ||
+                    hasPathSum(root.left, diff);
+        }
+    }
+}
