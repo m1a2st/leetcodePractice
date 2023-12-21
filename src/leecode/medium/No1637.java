ff6937a773a04b412378b7861b4a1da6a221864c
m1a2st <s7133700@gmail.com>
2023-12-21 21:10:07 +0800

    feat(Tree): practice
    662 1637

---
+++ src/leecode/medium/No1637.java
@@ -0,0 +1,21 @@
+package leecode.medium;
+
+import java.util.Arrays;
+
+public class No1637 {
+
+    class Solution {
+        public int maxWidthOfVerticalArea(int[][] points) {
+            int[] xArr = new int[points.length];
+            for (int i = 0; i < points.length; i++) {
+                xArr[i] = points[i][0];
+            }
+            Arrays.sort(xArr);
+            int ans = 0;
+            for (int i = 0; i < xArr.length - 1; i++) {
+                ans = Math.max(ans, xArr[i + 1] - xArr[i]);
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/leecode/medium/No662.java
@@ -0,0 +1,65 @@
+package leecode.medium;
+
+import kotlin.Pair;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class No662 {
+
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int widthOfBinaryTree(TreeNode root) {
+            if (root == null)
+                return 0;
+
+            int ans = 0;
+            Deque<Pair> q = new ArrayDeque<>(); // {node, index}
+            q.offer(new Pair(root, 1));
+
+            while (!q.isEmpty()) {
+                int offset = q.peekFirst().value * 2;
+                ans = Math.max(ans, q.peekLast().value - q.peekFirst().value + 1);
+                for (int sz = q.size(); sz > 0; --sz) {
+                    TreeNode node = q.peekFirst().key;
+                    int index = q.pollFirst().value;
+                    if (node.left != null)
+                        q.offer(new Pair(node.left, index * 2 - offset));
+                    if (node.right != null)
+                        q.offer(new Pair(node.right, index * 2 + 1 - offset));
+                }
+            }
+            return ans;
+        }
+
+        class Pair {
+            TreeNode key;
+            int value;
+
+            public Pair(TreeNode key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+    }
+}
