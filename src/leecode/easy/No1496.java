034cf8484fafdb96d25d4c19a7030ff99856b31c
m1a2st <s7133700@gmail.com>
2023-12-23 09:43:41 +0800

    feat(Tree): practice
    1496 96

---
+++ src/leecode/easy/No1496.java
@@ -0,0 +1,33 @@
+package leecode.easy;
+
+public class No1496 {
+
+    class Solution {
+        public boolean isPathCrossing(String path) {
+            int x = 100, y = 100;
+            boolean[][] visited = new boolean[200][200];
+            visited[x][y] = true;
+            for (char c : path.toCharArray()) {
+                switch (c) {
+                    case 'N':
+                        y++;
+                        break;
+                    case 'S':
+                        y--;
+                        break;
+                    case 'E':
+                        x++;
+                        break;
+                    case 'W':
+                        x--;
+                        break;
+                }
+                if (visited[x][y]) {
+                    return true;
+                }
+                visited[x][y] = true;
+            }
+            return false;
+        }
+    }
+}
