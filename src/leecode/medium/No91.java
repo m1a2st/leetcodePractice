cbd0869b771698a6ded84114ab3f5ac4ddb5e212
m1a2st <s7133700@gmail.com>
2023-12-26 20:47:02 +0800

    feat(Tree): practice
    91 129 1155

--- src/leecode/medium/No91.java
+++ src/leecode/medium/No91.java
@@ -33,4 +33,23 @@ public class No91 {
             return c1 == '1' || c1 == '2' && c2 < '7';
         }
     }
+
+    class SolutionNew {
+        public int numDecodings(String s) {
+            int len = s.length();
+            int[] dp = new int[len + 1];
+            dp[len] = 1;
+            dp[len - 1] = s.charAt(len - 1) == '0' ? 0 : 1;
+            for (int i = len - 2; i >= 0; --i) {
+                char iChar = s.charAt(i);
+                if (iChar != '0') {
+                    dp[i] += dp[i + 1];
+                }
+                if (iChar == '1' || (iChar == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+            return dp[0];
+        }
+    }
 }
