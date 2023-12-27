6a30ef63e83be7ca29a74914ed9f21dc89e42421
m1a2st <s7133700@gmail.com>
2023-12-27 20:49:06 +0800

    feat(Tree): practice
    509 1578

--- src/leecode/easy/No509.java
+++ src/leecode/easy/No509.java
@@ -1,6 +1,7 @@
 package leecode.easy;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -27,7 +28,24 @@ public class No509 {
         }
 
         public static int fib2(int n) {
-            if(n <= 1) return n;
+            if (n <= 1) return n;
+            res[0] = 0;
+            res[1] = 1;
+            for (int i = 2; i < n; i++) {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+            return res[n];
+        }
+    }
+
+    static class SolutionNew {
+
+        static int[] res = new int[31];
+
+        public int fib(int n) {
+            if (n <= 1) {
+                return 1;
+            }
             res[0] = 0;
             res[1] = 1;
             for (int i = 2; i < n; i++) {
