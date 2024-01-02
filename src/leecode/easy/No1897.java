c5db3d270c633c01f6c9203c208d19bbfe966e03
m1a2st <s7133700@gmail.com>
2024-01-02 21:25:33 +0800

    feat(Tree): practice
    455 894 951 1897 1993 2610

---
+++ src/leecode/easy/No1897.java
@@ -0,0 +1,21 @@
+package leecode.easy;
+
+import java.util.Arrays;
+
+public class No1897 {
+
+
+    class Solution {
+        public boolean makeEqual(String[] words) {
+            int[] count = new int[26];
+
+            for (final String word : words) {
+                for (final char c : word.toCharArray()) {
+                    ++count[c - 'a'];
+                }
+            }
+
+            return Arrays.stream(count).allMatch(c -> c % words.length == 0);
+        }
+    }
+}
