54976ab7026db8bb950a7ae8da2b6c114ab82af4
m1a2st <s7133700@gmail.com>
2024-01-09 20:39:12 +0800

    feat(Tree): practice
    226 872

--- src/leecode/easy/No226.java
+++ src/leecode/easy/No226.java
@@ -13,15 +13,13 @@ public class No226 {
     class Solution {
         // 定義：將以 root 為根的這棵二叉樹翻轉，返回翻轉後的二叉樹的根節點
         public TreeNode invertTree(TreeNode root) {
-            if(root == null){
+            if (root == null) {
                 return null;
             }
-            TreeNode right = root.right;
             TreeNode left = root.left;
-            // 利用函數定義，翻轉左右子樹，並且交換節點
-            root.left = invertTree(right);
+            TreeNode right = root.right;
             root.right = invertTree(left);
-            // 和定義邏輯自恰：以 root 為根的這棵二叉樹已經被翻轉，返回 root
+            root.left = invertTree(right);
             return root;
         }
     }
