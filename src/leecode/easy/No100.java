1e7f3ef3043d06c3bfaaa6b383432b3985e5a70e
m1a2st <s7133700@gmail.com>
2024-01-17 21:42:03 +0800

    feat(Tree): practice
    100 1207

--- src/leecode/easy/No100.java
+++ src/leecode/easy/No100.java
@@ -19,4 +19,19 @@ public class No100 {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
     }
+
+    class SolutionNew {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
 }
---
+++ src/leecode/easy/No1207.java
@@ -0,0 +1,17 @@
+package leecode.easy;
+
+import java.util.*;
+
+public class No1207 {
+
+    class Solution {
+        public boolean uniqueOccurrences(int[] arr) {
+            HashMap<Integer, Integer> ans = new HashMap<>();
+            for (int i : arr) {
+                ans.merge(i, 1, Integer::sum);
+            }
+            Set<Integer> set = new HashSet<>(ans.values());
+            return ans.size() == set.size();
+        }
+    }
+}
