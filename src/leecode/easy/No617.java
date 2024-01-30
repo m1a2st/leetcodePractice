4519d872bf4d020d32a95b8903bf0ef094c87319
m1a2st <s7133700@gmail.com>
2024-01-30 20:51:34 +0800

    feat(Tree): practice
    617

--- src/leecode/easy/No617.java
+++ src/leecode/easy/No617.java
@@ -23,6 +23,27 @@ public class No617 {
         }
     }
 
+    class SolutionNew {
+        public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+            return traversal(root1, root2);
+        }
+
+        private TreeNode traversal(TreeNode root1, TreeNode root2) {
+            if (root1 == null && root2 == null) {
+                return null;
+            }
+            if (root1 == null) {
+                return root2;
+            }
+            if (root2 == null) {
+                return root1;
+            }
+            return new TreeNode(root1.val + root2.val,
+                    traversal(root1.left, root2.left),
+                    traversal(root1.right, root2.right));
+        }
+    }
+
     class Solution {
         public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
             if (root1 == null && root2 == null) {
