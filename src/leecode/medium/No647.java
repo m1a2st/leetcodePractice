e3c3d8373b01b74a1943a604ac9549c58f5df0f5
m1a2st <s7133700@gmail.com>
2024-02-10 09:37:14 +0800

    feat(Tree): practice
    647

--- src/leecode/medium/No647.java
+++ src/leecode/medium/No647.java
@@ -21,7 +21,7 @@ public class No647 {
 
         private int extendPalindromes(String s, int left, int right) {
             int count = 0;
-            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
                 count++;
                 left--;
                 right++;
@@ -29,4 +29,26 @@ public class No647 {
             return count;
         }
     }
+
+    class SolutionNew {
+        public int countSubstrings(String s) {
+            int ans = 0;
+            for (int i = 0; i < s.length(); i++) {
+                ans += extendPalindromes(s, i, i); // 以 s[i] 为中心的奇数长度回文子串
+                ans += extendPalindromes(s, i, i + 1); // 以 s[i] 和 s[i+1] 为中心的偶数长度回文子串
+            }
+            return ans;
+        }
+
+        private int extendPalindromes(String s, int left, int right) {
+            int count = 0;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                ++count;
+                --left;
+                ++right;
+            }
+            return count;
+        }
+    }
+
 }
