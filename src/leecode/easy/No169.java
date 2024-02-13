323e71d982dffd47d2996d93f5b5419f72fd7968
m1a2st <s7133700@gmail.com>
2024-02-13 20:57:04 +0800

    feat(Tree): practice dp
    169 2108

--- src/leecode/easy/No169.java
+++ src/leecode/easy/No169.java
@@ -7,7 +7,7 @@ public class No169 {
             int count = 0;
             int candidate = 0;
             for (int num : nums) {
-                if(count == 0){
+                if (count == 0) {
                     candidate = num;
                 }
                 count += (num == candidate) ? 1 : -1;
@@ -15,4 +15,20 @@ public class No169 {
             return candidate;
         }
     }
+
+    class SolutionNew {
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            int ans = 0;
+            for (int num : nums) {
+                if (count == 0) {
+                    ans = num;
+                }
+                count += (ans == num) ? 1 : -1;
+            }
+            return ans;
+        }
+    }
+
+
 }
---
+++ src/leecode/easy/No2108.java
@@ -0,0 +1,28 @@
+package leecode.easy;
+
+public class No2108 {
+
+    class Solution {
+        public String firstPalindrome(String[] words) {
+            for (String word : words) {
+                if (isPalindromic(word)) {
+                    return word;
+                }
+            }
+            return "";
+        }
+
+        public boolean isPalindromic(String s) {
+            int left = 0;
+            int right = s.length() - 1;
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+            return true;
+        }
+    }
+}
