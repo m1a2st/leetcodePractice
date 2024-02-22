fc4d27fb13696fb9a1c8be817799dbcaddce70d7
m1a2st <s7133700@gmail.com>
2024-02-22 08:31:43 +0800

    feat(Tree): practice dp
    300 201

--- src/leecode/medium/No300.java
+++ src/leecode/medium/No300.java
@@ -65,4 +65,26 @@ public class No300 {
             return res;
         }
     }
+
+    class SolutionDpTwo {
+
+        int[] dp;
+
+        public int lengthOfLIS(int[] nums) {
+            dp = new int[nums.length];
+            Arrays.fill(dp, 1);
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            int res = 0;
+            for (int i : dp) {
+                res = Math.max(res, i);
+            }
+            return res;
+        }
+    }
 }
