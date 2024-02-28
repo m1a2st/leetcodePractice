8323d9f45b1cdf034c854b4be3a4d4fe852020e0
m1a2st <s7133700@gmail.com>
2024-02-28 23:29:20 +0900

    feat(Tree): practice graph
    2709 513

--- src/leecode/medium/No513.java
+++ src/leecode/medium/No513.java
@@ -45,4 +45,27 @@ public class No513 {
             traversal(root.right, depth + 1);
         }
     }
+
+    class SolutionNew {
+
+        int ans = 0;
+        int maxDepth = 0;
+
+        public int findBottomLeftValue(TreeNode root) {
+            traversal(root, 1);
+            return ans;
+        }
+
+        private void traversal(TreeNode root, int depth) {
+            if (root == null) {
+                return;
+            }
+            if (depth > maxDepth) {
+                maxDepth = depth;
+                ans = root.val;
+            }
+            traversal(root.left, depth + 1);
+            traversal(root.right, depth + 1);
+        }
+    }
 }
