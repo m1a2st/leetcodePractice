39942d8ef0ab3521d32bf42394e78f4c1a58c3a3
m1a2st <s7133700@gmail.com>
2024-03-20 22:10:59 +0800

    feat(daily): practice
    102. 950

--- src/leetcode/medium/No102.java
+++ src/leetcode/medium/No102.java
@@ -1,6 +1,9 @@
 package leetcode.medium;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * @Author m1a2st
@@ -33,6 +36,58 @@ public class No102 {
                 res.add(level);
             }
             return res;
+        }
+    }
+
+    class SolutionBfs {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> ans = new ArrayList<>();
+            Queue<TreeNode> queue = new ArrayDeque<>();
+            if (root == null) {
+                return ans;
+            }
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                ans.add(level);
+            }
+            return ans;
+        }
+    }
+
+    class SolutionDfs {
+        List<List<Integer>> ans = new ArrayList<>();
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            if (root == null) {
+                return ans;
+            }
+            traversal(root, 0);
+            return ans;
+        }
+
+        private void traversal(TreeNode root, int level) {
+            if (root != null) {
+                if (level == ans.size()) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(root.val);
+                    ans.add(list);
+                } else {
+                    ans.get(level).add(root.val);
+                }
+                traversal(root.left, level + 1);
+                traversal(root.right, level + 1);
+            }
         }
     }
 }
--- src/leetcode/medium/No950.java
+++ src/leetcode/medium/No950.java
@@ -1,19 +1,64 @@
 package leetcode.medium;
 
-import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.LinkedList;
 
 public class No950 {
 
-    // TODO not completed
+    @Test
+    public void test() {
+        new SolutionWorth().deckRevealedIncreasing(new int[]{1, 2, 3, 4, 5});
+    }
+
     class Solution {
         public int[] deckRevealedIncreasing(int[] deck) {
             Arrays.sort(deck);
             int[] res = new int[deck.length];
-            int di = 0;
-            for (int i = 0; i < res.length; i += 2) {
-                res[i] = deck[di++];
+            Queue<Integer> ans = new LinkedList<>();
+
+            for (int i = 0; i < deck.length; i++) {
+                ans.offer(i);
+            }
+
+            for (int card : deck) {
+                res[ans.poll()] = card;
+                if (!ans.isEmpty()) {
+                    ans.offer(ans.poll());
+                }
+            }
+
+            return res;
+        }
+    }
+
+    class SolutionWorth {
+        public int[] deckRevealedIncreasing(int[] deck) {
+            Arrays.sort(deck);
+            int[] res = new int[deck.length];
+            ArrayDeque<Integer> ans = new ArrayDeque<>();
+            for (int i = deck.length - 1; i >= 0; i--) {
+                List<Integer> list = new LinkedList<>();
+                while (ans.size() > 1) {
+                    list.add(ans.poll());
+                }
+                ans.push(deck[i]);
+                ans.addAll(list);
+            }
+            for (int i = 0; i < res.length; i++) {
+                res[i] = ans.poll();
             }
             return res;
         }
     }
 }
+
+// 1 2 3
+// 1 3 2
+// 1 2 3 4
+// 1 3 2 4
+// 1 2 3 4 5
+// 1 5 2 4 3
+// 1 2 3 4 5 6
+// 1 4 2 6 3 5
