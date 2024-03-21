7c7680ab81542d98421f9ebd79f94e13050365e8
m1a2st <s7133700@gmail.com>
2024-03-21 08:46:59 +0800

    feat(daily): practice
    206

--- src/leetcode/easy/No206.java
+++ src/leetcode/easy/No206.java
@@ -2,11 +2,13 @@ package leetcode.easy;
 
 import leetcode.medium.ListNode;
 
+import java.util.Stack;
+
 public class No206 {
 
     static class SolutionOld {
         public ListNode reverseList(ListNode head) {
-            if(head == null || head.next == null){
+            if (head == null || head.next == null) {
                 return head;
             }
             ListNode last = reverseList(head.next);
@@ -18,7 +20,38 @@ public class No206 {
 
     static class Solution {
         public ListNode reverseList(ListNode head) {
-            if(head == null || head.next == null){
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode last = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return last;
+        }
+    }
+
+
+    class SolutionNoRecursive {
+        public ListNode reverseList(ListNode head) {
+            Stack<Integer> stack = new Stack<>();
+            while (head != null) {
+                stack.push(head.val);
+                head = head.next;
+            }
+            ListNode dummy = new ListNode(-1);
+            ListNode cur = dummy;
+            while (!stack.isEmpty()) {
+                ListNode node = new ListNode(stack.pop());
+                cur.next = node;
+                cur = cur.next;
+            }
+            return dummy.next;
+        }
+    }
+
+    class SolutionRecursive {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
                 return head;
             }
             ListNode last = reverseList(head.next);
