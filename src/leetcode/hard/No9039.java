1000ae40261beecf6b3ac1f8710b630f3719da04
m1a2st <s7133700@gmail.com>
2024-03-26 13:16:27 +0800

    feat(daily): practice
    41, 45, 69, 9039

---
+++ src/leetcode/hard/No9039.java
@@ -0,0 +1,74 @@
+package leetcode.hard;
+
+import org.junit.jupiter.api.Test;
+
+// TODO 太難了，根本想不到
+public class No9039 {
+
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        String[] wordsContainer = {"abcd", "bcd", "xbcd"};
+        String[] wordsQuery = {"cd","bcd","xyz"};
+        int[] ans = solution.stringIndices(wordsContainer, wordsQuery);
+        for (int an : ans) {
+            System.out.println(an);
+        }
+    }
+
+    class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public int length = Integer.MAX_VALUE;
+        public int index = -1;
+    }
+
+    class Solution {
+        private TrieNode root = new TrieNode();
+
+        public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
+            int[] ans = new int[wordsQuery.length];
+            int minIndex = 0;
+
+            for (int i = 0; i < wordsContainer.length; ++i) {
+                insert(wordsContainer[i], i);
+                if (wordsContainer[i].length() < wordsContainer[minIndex].length()) {
+                    minIndex = i;
+                }
+            }
+
+            for (int i = 0; i < wordsQuery.length; ++i) {
+                final int index = search(wordsQuery[i]);
+                ans[i] = index == -1 ? minIndex : index;
+            }
+
+            return ans;
+        }
+
+
+        private void insert(String word, int index) {
+            TrieNode node = root;
+            for (int i = word.length() - 1; i >= 0; --i) {
+                final int j = word.charAt(i) - 'a';
+                if (node.children[j] == null) {
+                    node.children[j] = new TrieNode();
+                }
+                node = node.children[j];
+                if (node.length > word.length()) {
+                    node.length = word.length();
+                    node.index = index;
+                }
+            }
+        }
+
+        private int search(final String word) {
+            TrieNode node = root;
+            for (int i = word.length() - 1; i >= 0; --i) {
+                final int j = word.charAt(i) - 'a';
+                if (node.children[j] == null)
+                    return node.index;
+                node = node.children[j];
+            }
+            return node.index;
+        }
+    }
+}
--- src/leetcode/medium/No45.java
+++ src/leetcode/medium/No45.java
@@ -1,5 +1,7 @@
 package leetcode.medium;
 
+import java.util.Arrays;
+
 public class No45 {
 
 
@@ -8,11 +10,12 @@ public class No45 {
             int ans = 0;
             int end = 0;
             int farthest = 0;
+            int len = nums.length - 1;
 
             // Implicit BFS
-            for (int i = 0; i < nums.length - 1; ++i) {
+            for (int i = 0; i < len; ++i) {
                 farthest = Math.max(farthest, i + nums[i]);
-                if (farthest >= nums.length - 1) {
+                if (farthest >= len) {
                     ++ans;
                     break;
                 }
@@ -23,6 +26,23 @@ public class No45 {
             }
 
             return ans;
+        }
+    }
+
+    class SolutionNew {
+        public int jump(int[] nums) {
+            int len = nums.length;
+            int[] dp = new int[len];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+            for (int i = 0; i < len; ++i) {
+                for (int j = 1; j <= nums[i]; ++j) {
+                    if (i + j < len) {
+                        dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+                    }
+                }
+            }
+            return dp[len - 1];
         }
     }
 }
