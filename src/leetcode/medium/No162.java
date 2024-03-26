a8bb03a585ec1f72eeac4af8be0880d57097b7ba
m1a2st <s7133700@gmail.com>
2024-03-26 20:58:01 +0800

    feat(daily): practice
    162

--- src/leetcode/medium/No162.java
+++ src/leetcode/medium/No162.java
@@ -25,4 +25,20 @@ public class No162 {
             return left;
         }
     }
+
+    class SolutionNew {
+        public int findPeakElement(int[] nums) {
+            int left = 0, right = nums.length;
+            while (left < right) {
+                int mid = (right - left) / 2 + left;
+                int midR = mid + 1;
+                if (nums[mid] > nums[midR]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+    }
 }
