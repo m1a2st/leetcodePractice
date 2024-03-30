65f22ceb203fd9de7d6ca9699670519eefeac387
m1a2st <s7133700@gmail.com>
2024-03-30 21:02:17 +0800

    feat(daily): practice
    12, 27, 88, 135, 151

--- src/leetcode/easy/No27.java
+++ src/leetcode/easy/No27.java
@@ -24,13 +24,12 @@ public class No27 {
         }
     }
 
-    static class SolutionNew {
+    class SolutionNew {
         public static int removeElement(int[] nums, int val) {
             int count = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if(nums[i] != val){
-                    nums[count] = nums[i];
-                    count++;
+            for (int i = 0; i < nums.length; ++i) {
+                if (nums[i] != val) {
+                    nums[count++] = nums[i];
                 }
             }
             return count;
---
+++ src/leetcode/medium/No151.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+public class No151 {
+
+    class Solution {
+        public String reverseWords(String s) {
+            s = s.trim();
+            String[] split = s.split("\\s+");
+            StringBuilder sb = new StringBuilder();
+            for (int i = split.length - 1; i >= 0; i--) {
+                sb.append(split[i]).append(" ");
+            }
+            return sb.substring(0, sb.length() - 1);
+        }
+    }
+}
