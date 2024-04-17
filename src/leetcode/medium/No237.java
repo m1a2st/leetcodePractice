5baa01d1e40d9e5845b245accd5556d6f3a3edad
m1a2st <s7133700@gmail.com>
2024-04-17 22:16:25 +0800

    feat(daily): practice
    237

--- src/leetcode/medium/No237.java
+++ src/leetcode/medium/No237.java
@@ -16,100 +16,99 @@ public class No237 {
         node.next = node.next.next;
     }
 
-}
 
+    static class LinkedList {
+        int length;
+        ListNode head;
 
-class LinkedList {
-    int length;
-    ListNode head;
+        public LinkedList() {
+        }
 
-    public LinkedList() {
-    }
+        public LinkedList(int length, ListNode head) {
+            this.length = length;
+            this.head = head;
+        }
 
-    public LinkedList(int length, ListNode head) {
-        this.length = length;
-        this.head = head;
-    }
+        public void push(int val) {
+            ListNode newNode = new ListNode(val);
+            if (this.head == null) {
+                this.head = newNode;
+            } else {
+                ListNode currentNode = this.head;
+                while (currentNode.next != null) {
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = newNode;
+            }
+            this.length++;
+        }
 
-    public void push(int val) {
-        ListNode newNode = new ListNode(val);
-        if (this.head == null) {
-            this.head = newNode;
-        } else {
+        public void unshift(int val) {
+            if (this.head == null) {
+                this.head = new ListNode(val);
+            } else {
+                ListNode temp = this.head;
+                this.head = new ListNode(val);
+                this.head.next = temp;
+            }
+            length++;
+        }
+
+        public ListNode pop() {
+            if (this.head == null) {
+                return null;
+            } else if (this.length == 1) {
+                ListNode temp = this.head;
+                this.head = null;
+                length = 0;
+                return temp;
+            } else {
+                ListNode currentNode = this.head;
+                for (int i = 1; i <= length - 2; i++) {
+                    currentNode = currentNode.next;
+                }
+                ListNode temp = currentNode.next;
+                currentNode.next = null;
+                this.length--;
+                return temp;
+            }
+        }
+
+        public void removeAt(int index) {
+            if (index > this.length - 1 || index < 0) {
+
+            } else if (index == 0) {
+                this.shift();
+            } else if (index == this.length - 1) {
+                this.pop();
+            }
+        }
+
+        public ListNode shift() {
+            if (this.head == null) {
+                return null;
+            } else if (this.length == 1) {
+                ListNode temp = this.head;
+                this.head = null;
+                this.length = 0;
+                return temp;
+            } else {
+                ListNode temp = this.head;
+                this.head = this.head.next;
+                this.length--;
+                return temp;
+            }
+        }
+
+        public void printAll() {
+            if (this.length == 0) {
+                System.out.println("Nothing in this linked list");
+            }
             ListNode currentNode = this.head;
-            while (currentNode.next != null) {
+            while (currentNode != null) {
+                System.out.println(currentNode.val);
                 currentNode = currentNode.next;
             }
-            currentNode.next = newNode;
-        }
-        this.length++;
-    }
-
-    public void unshift(int val) {
-        if (this.head == null) {
-            this.head = new ListNode(val);
-        } else {
-            ListNode temp = this.head;
-            this.head = new ListNode(val);
-            this.head.next = temp;
-        }
-        length++;
-    }
-
-    public ListNode pop() {
-        if (this.head == null) {
-            return null;
-        } else if (this.length == 1) {
-            ListNode temp = this.head;
-            this.head = null;
-            length = 0;
-            return temp;
-        } else {
-            ListNode currentNode = this.head;
-            for (int i = 1; i <= length - 2; i++) {
-                currentNode = currentNode.next;
-            }
-            ListNode temp = currentNode.next;
-            currentNode.next = null;
-            this.length--;
-            return temp;
-        }
-    }
-
-    public void removeAt(int index) {
-        if (index > this.length - 1 || index < 0) {
-
-        } else if (index == 0) {
-            this.shift();
-        } else if (index == this.length - 1) {
-            this.pop();
-        }
-    }
-
-    public ListNode shift() {
-        if (this.head == null) {
-            return null;
-        } else if (this.length == 1) {
-            ListNode temp = this.head;
-            this.head = null;
-            this.length = 0;
-            return temp;
-        } else {
-            ListNode temp = this.head;
-            this.head = this.head.next;
-            this.length--;
-            return temp;
-        }
-    }
-
-    public void printAll() {
-        if (this.length == 0) {
-            System.out.println("Nothing in this linked list");
-        }
-        ListNode currentNode = this.head;
-        while (currentNode != null) {
-            System.out.println(currentNode.val);
-            currentNode = currentNode.next;
         }
     }
 }
