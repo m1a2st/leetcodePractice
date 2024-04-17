fab452bcdc565b6e04e80579a610a78f8223f13f
m1a2st <s7133700@gmail.com>
2024-04-17 20:53:41 +0800

    feat(daily): practice
    123

---
+++ src/leetcode/medium/No123.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+public class No123 {
+
+
+    @Test
+    public void test() {
+        new Solution().maxProfit(new int[]{1, 2, 1, 4, 2, 5});
+    }
+
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int sellTwo = 0;
+            int holdTwo = Integer.MIN_VALUE;
+            int sellOne = 0;
+            int holdOne = Integer.MIN_VALUE;
+
+            for (int price : prices) {
+                sellTwo = Math.max(sellTwo, holdTwo + price);
+                holdTwo = Math.max(holdTwo, sellOne - price);
+                sellOne = Math.max(sellOne, holdOne + price);
+                holdOne = Math.max(holdOne, -price);
+            }
+
+            return sellTwo;
+        }
+    }
+}
