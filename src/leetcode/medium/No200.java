27499e7e114421a8398852ae9cb63f26900325a6
m1a2st <s7133700@gmail.com>
2024-04-19 08:21:24 +0800

    feat(daily): practice
    200

--- src/leetcode/medium/No200.java
+++ src/leetcode/medium/No200.java
@@ -1,5 +1,7 @@
 package leetcode.medium;
 
+import org.junit.Test;
+
 /**
  * @Author
  * @Date
@@ -7,42 +9,23 @@ package leetcode.medium;
  * @Description
  */
 public class No200 {
-    static class Solution {
-        public int numIslands(char[][] grid) {
-            if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-            int count = 0;
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[i].length; j++) {
-                    if (grid[i][j] == '1') {
-                        dfs(grid, i, j);
-                        count++;
-                    }
-                }
-            }
-            return count;
-        }
 
-        public void dfs(char[][] grid, int i, int j) {
-            if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
-                return;
-            }
-            grid[i][j] = '0';
-
-            dfs(grid, i + 1, j);
-            dfs(grid, i - 1, j);
-            dfs(grid, i, j + 1);
-            dfs(grid, i, j - 1);
-        }
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        solution.numIslands(new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}});
     }
 
-    static class SolutionNew {
-
-        int ans = 0;
+    class Solution {
 
         public int numIslands(char[][] grid) {
-            if (grid == null || grid.length == 0 || grid[0].length == 0) return ans;
+            int ans = 0;
             for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[i].length; j++) {
+                for (int j = 0; j < grid[0].length; j++) {
                     if (grid[i][j] == '1') {
                         dfs(grid, i, j);
                         ans++;
@@ -53,15 +36,17 @@ public class No200 {
         }
 
         private void dfs(char[][] grid, int i, int j) {
-            // 終止條件
-            if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
+            if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
                 return;
             }
-            grid[i][j] = '0';
-
+            if (grid[i][j] == '1') {
+                grid[i][j] = '0';
+            } else {
+                return;
+            }
             dfs(grid, i + 1, j);
-            dfs(grid, i, j + 1);
             dfs(grid, i - 1, j);
+            dfs(grid, i, j + 1);
             dfs(grid, i, j - 1);
         }
     }
