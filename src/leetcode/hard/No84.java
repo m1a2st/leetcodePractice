ece5cebf1aecf43d874f71501490380e6f1bf247
m1a2st <s7133700@gmail.com>
2024-04-29 20:19:39 +0800

    feat(sort): practice
    84

--- src/leetcode/hard/No84.java
+++ src/leetcode/hard/No84.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Queue;
 
 public class No84 {
 
@@ -25,61 +26,26 @@ public class No84 {
         }
     }
 
-    class SolutionBad {
-        public int largestRectangleArea(int[] heights) {
-            int ans = 0;
-            int len = heights.length;
-            for (int i = 0; i < len; i++) {
-                ans = Math.max(ans, heights[i]);
-                for (int j = 1; j <= heights[i]; j++) {
-                    int next = i;
-                    while (++next < len && heights[next] >= j) {
-
-                    }
-                    ans = Math.max(ans, (next - i) * j);
-                }
-            }
-            return ans;
-        }
-    }
-
     @Test
     public void test() {
-        new SolutionPQ().largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3});
+        new Solution().largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3});
     }
 
-
-    class SolutionPQ {
+    class SolutionNew {
         public int largestRectangleArea(int[] heights) {
             int ans = 0;
-            Deque<Integer> stack = new ArrayDeque<>();
-
-            for (int i = 0; i <= heights.length; ++i) {
-                while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i])) {
-                    int h = heights[stack.pop()];
-                    int w = stack.isEmpty() ? i : i - stack.peek() - 1;
+            Deque<Integer> cache = new ArrayDeque<>();
+            int len = heights.length;
+            for (int i = 0; i <= len; i++) {
+                while (!cache.isEmpty() && (i == len || heights[cache.peek()] > heights[i])) {
+                    int h = heights[cache.pop()];
+                    int w = cache.isEmpty() ? i : i - cache.peek() - 1;
                     ans = Math.max(ans, h * w);
                 }
-                stack.push(i);
-            }
-
-            return ans;
-        }
-    }
-
-    class SolutionP {
-        public int largestRectangleArea(int[] heights) {
-            int ans = 0;
-            Deque<Integer> stack = new ArrayDeque<>();
-            for (int i = 0; i <= heights.length; i++) {
-                while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i])) {
-                    int h = heights[stack.pop()];
-                    int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-                    ans = Math.max(ans, h * w);
-                }
-                stack.push(i);
+                cache.push(i);
             }
             return ans;
         }
     }
+
 }
