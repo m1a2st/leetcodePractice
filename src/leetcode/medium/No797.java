c15399e8e0b2d2e93742aebefa75a2d9c5556ca7
m1a2st <s7133700@gmail.com>
2024-04-29 22:16:13 +0800

    feat(sort): practice
    797

--- src/leetcode/medium/No797.java
+++ src/leetcode/medium/No797.java
@@ -9,11 +9,6 @@ import java.util.Queue;
 
 public class No797 {
 
-    @Test
-    public void test() {
-
-    }
-
     class Solution {
         List<List<Integer>> ans = new ArrayList<>();
 
@@ -32,6 +27,32 @@ public class No797 {
                 }
             }
             path.remove(path.size() - 1); // 回溯時刪除最後一個節點
+        }
+    }
+
+    @Test
+    public void test() {
+        new SolutionNew().allPathsSourceTarget(new int[][]{{1, 2}, {3}, {3}, {}});
+    }
+
+    class SolutionNew {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            dfs(graph, 0, new ArrayList<>());
+            return ans;
+        }
+
+        private void dfs(int[][] graph, int start, ArrayList<Integer> path) {
+            path.add(start);
+            if (start == graph.length - 1) {
+                ans.add(new ArrayList<>(path));
+            }
+            for (int i : graph[start]) {
+                dfs(graph, i, path);
+            }
+            path.remove(path.size() - 1);
         }
     }
 }
