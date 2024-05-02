f6d2f4e6b5ce0abd48037d191e8b30910169d1c3
m1a2st <s7133700@gmail.com>
2024-05-02 20:53:28 +0800

    feat(daily): practice
    239, 295

--- src/leetcode/hard/No239.java
+++ src/leetcode/hard/No239.java
@@ -23,7 +23,7 @@ public class No239 {
                         new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
     }
 
-    class Solution{
+    class Solution {
         public int[] maxSlidingWindow(int[] nums, int k) {
             int len = nums.length;
             int[] ans = new int[len - k + 1];
@@ -38,6 +38,26 @@ public class No239 {
                 dq.offer(i);
                 if (i >= k - 1) {
                     ans[i - k + 1] = nums[dq.peek()];
+                }
+            }
+            return ans;
+        }
+    }
+
+    class SolutionNew {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int[] ans = new int[nums.length - k + 1];
+            Deque<Integer> queue = new ArrayDeque<>();
+            for (int i = 0; i < nums.length; i++) {
+                while (!queue.isEmpty() && queue.peek() < i - k + 1) {
+                    queue.pop();
+                }
+                while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                    queue.pollLast();
+                }
+                queue.offer(i);
+                if (i >= k - 1) {
+                    ans[i - k + 1] = nums[queue.peek()];
                 }
             }
             return ans;
