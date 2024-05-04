c61d32fee0747ae4d7412996c6e0e2fa4850c502
m1a2st <s7133700@gmail.com>
2024-05-04 20:32:58 +0800

    feat(daily): practice
    295, 297

--- src/leetcode/hard/No295.java
+++ src/leetcode/hard/No295.java
@@ -48,4 +48,27 @@ public class No295 {
         }
     }
 
+    class MedianFinderNew {
+
+        private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        public void addNum(int num) {
+            if (maxHeap.isEmpty() || num <= maxHeap.peek()) {
+                maxHeap.offer(num);
+            } else {
+                minHeap.offer(num);
+            }
+            if (minHeap.size() > maxHeap.size()) {
+                maxHeap.offer(minHeap.poll());
+            } else if (maxHeap.size() - minHeap.size() > 1) {
+                minHeap.offer(maxHeap.poll());
+            }
+        }
+
+        public double findMedian() {
+            return minHeap.size() == maxHeap.size() ? (minHeap.peek() + maxHeap.peek()) / 2.0 : maxHeap.peek();
+        }
+    }
+
 }
