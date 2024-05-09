a6cb4c812fde2405ad45259d99645cca04d3bc09
m1a2st <s7133700@gmail.com>
2024-05-09 20:12:19 +0800

    feat(daily): practice
    967

---
+++ src/leetcode/medium/No967.java
@@ -0,0 +1,55 @@
+package leetcode.medium;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class No967 {
+
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        for (int i : solution.numsSameConsecDiff(3, 7)) {
+            System.out.println(i);
+        }
+    }
+
+    class Solution {
+        Set<Integer> ans = new HashSet<>();
+
+        public int[] numsSameConsecDiff(int n, int k) {
+            for (int i = 1; i < 10; i++) {
+                backtracking(i, n - 1, k);
+            }
+            int[] res = new int[ans.size()];
+            int i = 0;
+            for (Integer an : ans) {
+                res[i++] = an;
+            }
+            return res;
+        }
+
+        private void backtracking(int num, int n, int k) {
+            if (n == 0) {
+                ans.add(num);
+                return;
+            }
+
+            int newNum = num * 10;
+            int compareNum = num % 10;
+            int addNext = compareNum + k;
+            if (addNext <= 9) {
+                newNum += addNext;
+                backtracking(newNum, n - 1, k);
+                newNum -= addNext;
+            }
+
+            int subNext = compareNum - k;
+            if (subNext >= 0) {
+                newNum += subNext;
+                backtracking(newNum, n - 1, k);
+            }
+        }
+    }
+}
