e6c79431bec111f35474a393053132449e389ed4
m1a2st <s7133700@gmail.com>
2024-05-24 18:54:53 +0800

    feat(daily): practice
    105

--- src/leetcode/medium/No105.java
+++ src/leetcode/medium/No105.java
@@ -37,14 +37,22 @@ public class No105 {
             return build(preorder, 0, preorder.length - 1, 0);
         }
 
+        /**
+         *
+         * @param preStart 前序遍歷的起始索引
+         * @param preEnd  前序遍歷的結束索引
+         * @param inStart 中序遍歷的起始索引
+         */
         private TreeNode build(int[] preorder, int preStart, int preEnd, int inStart) {
             // 終止條件為 前序遍歷的索引大於前序遍歷的長度
             if (preStart > preEnd) {
                 return null;
             }
-
+            // 前序遍歷的第一個節點為根節點
             final int rootVal = preorder[preStart];
+            // 根節點在中序遍歷的索引
             final int rootInIndex = inToIndex.get(rootVal);
+            // 左子樹的節點數量
             final int leftSize = rootInIndex - inStart;
 
             TreeNode root = new TreeNode(rootVal);
