0434e6093d7230e5215ed8de9a95bd9d9823189b
m1a2st <s7133700@gmail.com>
2024-05-27 08:41:22 +0800

    feat(daily): practice
    417

--- src/leetcode/medium/No417.java
+++ src/leetcode/medium/No417.java
@@ -1,5 +1,7 @@
 package leetcode.medium;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,19 @@ import java.util.List;
  * @Version v1.0
  */
 public class No417 {
+
+    @Test
+    public void test() {
+        int[][] heights = {
+                {1, 2, 2, 3, 5},
+                {3, 2, 3, 4, 4},
+                {2, 4, 5, 3, 1},
+                {6, 7, 1, 4, 5},
+                {5, 1, 1, 2, 4}
+        };
+        Solution solution = new Solution();
+        System.out.println(solution.pacificAtlantic(heights));
+    }
 
     class Solution {
         public List<List<Integer>> pacificAtlantic(int[][] heights) {
@@ -29,19 +44,24 @@ public class No417 {
                 dfs(heights, m - 1, j, 0, seenA);
             }
 
-            for (int i = 0; i < m; ++i)
-                for (int j = 0; j < n; ++j)
-                    if (seenP[i][j] && seenA[i][j])
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (seenP[i][j] && seenA[i][j]) {
                         ans.add(new ArrayList<>(Arrays.asList(i, j)));
+                    }
+                }
+            }
 
             return ans;
         }
 
         private void dfs(final int[][] heights, int i, int j, int h, boolean[][] seen) {
-            if (i < 0 || i == heights.length || j < 0 || j == heights[0].length)
+            if (i < 0 || i == heights.length || j < 0 || j == heights[0].length) {
                 return;
-            if (seen[i][j] || heights[i][j] < h)
+            }
+            if (seen[i][j] || heights[i][j] < h) {
                 return;
+            }
 
             seen[i][j] = true;
             dfs(heights, i + 1, j, heights[i][j], seen);
