02b902fc7ae4b83ae01fd67aa88ca2341b686dcb
m1a2st <s7133700@gmail.com>
2024-06-01 09:51:25 +0800

    feat(daily): practice
    3110

---
+++ src/leetcode/easy/No3110.java
@@ -0,0 +1,16 @@
+package leetcode.easy;
+
+public class No3110 {
+
+    class Solution {
+        public int scoreOfString(String s) {
+            char[] arr = s.toCharArray();
+            int len = arr.length;
+            int ans = 0;
+            for (int i = 1; i < len; ++i) {
+                ans += Math.abs(arr[i] - arr[i - 1]);
+            }
+            return ans;
+        }
+    }
+}
