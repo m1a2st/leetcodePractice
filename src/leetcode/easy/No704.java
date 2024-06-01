597ae8f6c3b4b240d54a13ece523078448c27198
m1a2st <s7133700@gmail.com>
2024-06-01 22:19:54 +0800

    feat(daily): practice
    704

--- src/leetcode/easy/No704.java
+++ src/leetcode/easy/No704.java
@@ -3,8 +3,8 @@ package leetcode.easy;
 public class No704 {
 
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 3, 5, 9, 12};
-        int target = 9;
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 5;
         int search = SolutionOld.search(nums, target);
         System.out.println(search);
     }
@@ -36,23 +36,6 @@ public class No704 {
                 if (num == target) {
                     return mid;
                 } else if (num > target) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            return -1;
-        }
-    }
-
-    class SolutionNew {
-        public int search(int[] nums, int target) {
-            int left = 0, right = nums.length;
-            while (left < right) {
-                int mid = (right - left) / 2 + left;
-                if (nums[mid] == target) {
-                    return mid;
-                } else if (nums[mid] > target) {
                     right = mid;
                 } else {
                     left = mid + 1;
