4f117d7dd4253cf5bec3fbc98cc26c49db8d408e
m1a2st <s7133700@gmail.com>
2024-06-02 10:01:23 +0800

    feat(daily): practice
    344

--- src/leetcode/easy/No344.java
+++ src/leetcode/easy/No344.java
@@ -1,5 +1,7 @@
 package leetcode.easy;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @Author m1a2st
  * @Date 2023/3/21
@@ -7,22 +9,23 @@ package leetcode.easy;
  */
 public class No344 {
 
-    public static void main(String[] args) {
-        Solution.reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
+    @Test
+    public void test() {
+        new Solution().reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
     }
 
-    static class Solution {
-        public static void reverseString(char[] s) {
-            int index = s.length - 1;
-            for (int i = 0; i < (s.length / 2); i++, index--) {
-                swap(s, i, index);
+    class Solution {
+        public void reverseString(char[] s) {
+            int left = 0, right = s.length - 1;
+            while (left <= right) {
+                swap(s, left++, right--);
             }
         }
 
-        private static void swap(char[] s, int i, int length) {
-            char c = s[i];
-            s[i] = s[length];
-            s[length] = c;
+        private void swap(char[] arr, int left, int right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
         }
     }
 }
