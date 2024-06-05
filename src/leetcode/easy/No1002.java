2a3e9f7dc8a974b0b7c8298bb87583020ff3bfa4
m1a2st <s7133700@gmail.com>
2024-06-05 09:17:40 +0800

    feat(daily): practice
    1002

--- src/leetcode/easy/No1002.java
+++ src/leetcode/easy/No1002.java
@@ -2,18 +2,11 @@ package leetcode.easy;
 
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class No1002 {
-
-    @Test
-    void test2() {
-        BigDecimal b = new BigDecimal("4");
-        System.out.println(b.subtract(new BigDecimal("2")).divide(new BigDecimal("2")));
-    }
 
     @Test
     void test() {
