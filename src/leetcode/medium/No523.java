aca6a88272dd56b46fd3769b105cd95eebd41992
m1a2st <s7133700@gmail.com>
2024-06-10 00:52:34 +0800

    feat(daily): practice
    523, 974

--- src/leetcode/medium/No523.java
+++ src/leetcode/medium/No523.java
@@ -3,13 +3,15 @@ package leetcode.medium;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class No523 {
 
     @Test
     public void test() {
-        Solution solution = new Solution();
+        SolutionNew solution = new SolutionNew();
         int[] nums = {6, 0, 14, 6, 7};
         System.out.println(solution.checkSubarraySum(nums, 7));
     }
@@ -37,4 +39,22 @@ public class No523 {
             return false;
         }
     }
+
+    class SolutionNew {
+        public boolean checkSubarraySum(int[] nums, int k) {
+            int n = nums.length;
+            int[] sum = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                sum[i] = sum[i - 1] + nums[i - 1];
+            }
+
+            Set<Integer> set = new HashSet<>();
+            for (int i = 2; i <= n; i++) {
+                set.add(sum[i - 2] % k);
+                if (set.contains(sum[i] % k)) return true;
+            }
+            return false;
+        }
+    }
+
 }
