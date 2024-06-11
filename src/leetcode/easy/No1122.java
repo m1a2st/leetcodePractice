180a19b9b7cd44c96beadcda72d1b822004d540d
m1a2st <s7133700@gmail.com>
2024-06-11 13:58:41 +0800

    feat(daily): practice
    1122

--- src/leetcode/easy/No1122.java
+++ src/leetcode/easy/No1122.java
@@ -4,7 +4,6 @@ public class No1122 {
     class Solution {
 
         public int[] relativeSortArray(int[] arr1, int[] arr2) {
-            int[] ans = new int[arr1.length];
             int index = 0;
             int[] cache = new int[1001];
             for (int num : arr1) {
@@ -13,18 +12,18 @@ public class No1122 {
             for (int num : arr2) {
                 int freq = cache[num];
                 for (int i = 0; i < freq; ++i) {
-                    ans[index++] = num;
+                    arr1[index++] = num;
                     cache[num]--;
                 }
             }
             for (int i = 0; i < 1001; i++) {
                 if (cache[i] != 0) {
                     for (int j = 0; j < cache[i]; ++j) {
-                        ans[index++] = i;
+                        arr1[index++] = i;
                     }
                 }
             }
-            return ans;
+            return arr1;
         }
     }
 }
