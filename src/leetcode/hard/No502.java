5f39bbac3a344d4aaa1f30e9da16546abc97fd0c
m1a2st <s7133700@gmail.com>
2024-06-15 09:10:40 +0800

    feat(daily): practice
    502

---
+++ src/leetcode/hard/No502.java
@@ -0,0 +1,33 @@
+package leetcode.hard;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class No502 {
+
+    class Solution {
+        public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+            int len = profits.length;
+            int[][] edge = new int[len][2];
+            for (int i = 0; i < len; i++) {
+                edge[i][0] = capital[i];
+                edge[i][1] = profits[i];
+            }
+            Arrays.sort(edge, Comparator.comparingInt(a -> a[0]));
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(a -> -a));
+            int i = 0;
+            for (int j = 0; j < k; j++) {
+                while (i < len && edge[i][0] <= w) {
+                    pq.offer(edge[i++][1]);
+                }
+                if (!pq.isEmpty()) {
+                    w += pq.poll();
+                } else {
+                    break;
+                }
+            }
+            return w;
+        }
+    }
+}
