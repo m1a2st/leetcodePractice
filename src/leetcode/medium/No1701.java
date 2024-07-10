4f53a093e309d5f0d857e2dbf96db31a3aaa4aed
m1a2st <s7133700@gmail.com>
2024-07-10 08:46:27 +0800

    feat(daily): practice
    1701

---
+++ src/leetcode/medium/No1701.java
@@ -0,0 +1,18 @@
+package leetcode.medium;
+
+public class No1701 {
+    
+    class Solution {
+        public double averageWaitingTime(int[][] customers) {
+            double wait = 0;
+            double curr = 0;
+
+            for (int[] c : customers) {
+                curr = Math.max(curr, 1.0 * c[0]) + c[1];
+                wait += curr - c[0];
+            }
+
+            return wait / customers.length;
+        }
+    }
+}
