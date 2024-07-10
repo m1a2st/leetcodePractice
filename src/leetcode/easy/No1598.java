edfb8e1ad1fe0339e199da6eb566466c10e1dffb
m1a2st <s7133700@gmail.com>
2024-07-10 08:21:58 +0800

    feat(daily): practice
    1598

---
+++ src/leetcode/easy/No1598.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class No1598 {
+
+    class Solution {
+        public int minOperations(String[] logs) {
+            int ans = 0;
+            for (String log : logs) {
+                if (log.contains("..") && ans != 0) {
+                    ans--;
+                } else if (log.contains(".")) {
+
+                } else {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
