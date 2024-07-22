fa915f91a51508cea654b7fd360dce9fef043dc6
m1a2st <s7133700@gmail.com>
2024-07-22 21:23:24 +0800

    feat(daily): practice
    2418

---
+++ src/leetcode/easy/No2418.java
@@ -0,0 +1,38 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+public class No2418 {
+
+    class Solution {
+        public String[] sortPeople(String[] names, int[] heights) {
+            int n = names.length;
+            Person[] people = new Person[n];
+            for (int i = 0; i < n; ++i)
+                people[i] = new Person(names[i], heights[i]);
+            
+            Arrays.sort(people);
+            
+            String[] ans = new String[n];
+            for (int i = 0; i < n; ++i)
+                ans[i] = people[i].name;
+            
+            return ans;
+        }
+        
+        class Person implements Comparable<Person> {
+            String name;
+            int height;
+            
+            Person(String name, int height) {
+                this.name = name;
+                this.height = height;
+            }
+
+            @Override
+            public int compareTo(Person o) {
+                return this.height - o.height;
+            }
+        }
+    }
+}
