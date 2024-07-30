760e03f838eb6fba3515856244bf1865106c3774
m1a2st <s7133700@gmail.com>
2024-07-30 16:30:22 +0800

    feat(daily): practice
    1653

---
+++ src/leetcode/medium/No1653.java
@@ -0,0 +1,22 @@
+package leetcode.medium;
+
+public class No1653 {
+
+    class Solution {
+        public int minimumDeletions(String s) {
+            int dp = 0;
+            int countB = 0;
+
+            for (final char c : s.toCharArray()) {
+                if (c == 'a')
+                    // 1. Delete 'a'.
+                    // 2. Keep 'a' and delete the previous 'b's.
+                    dp = Math.min(dp + 1, countB);
+                else
+                    ++countB;
+            }
+
+            return dp;
+        }
+    }
+}
