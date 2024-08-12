3122701b966df697124239531275ea5334c3f6c7
m1a2st <s7133700@gmail.com>
2024-08-12 08:53:11 +0800

    feat(junit): daily
    703

--- src/leetcode/easy/No703.java
+++ src/leetcode/easy/No703.java
@@ -1,17 +1,14 @@
 package leetcode.easy;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-/**
- * @Author
- * @Date
- * @Version
- * @Description
- */
 public class No703 {
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
         System.out.println(kthLargest.add(3));   // return 4
         System.out.println(kthLargest.add(5));   // return 5
@@ -21,42 +18,26 @@ public class No703 {
 
     }
 
-    static class KthLargestOld {
+    class KthLargest {
 
-        private final Queue<Integer> pq = new PriorityQueue<>();
-        private final int k;
-
-        public KthLargestOld(int k, int[] nums) {
-            this.k = k;
-            for (int i : nums) {
-                pq.add(i);
-                if (pq.size() > k) pq.poll();
-            }
-        }
-
-        public int add(int val) {
-            pq.add(val);
-            if (pq.size() > k) pq.poll();
-            return pq.peek();
-        }
-    }
-
-    static class KthLargest {
-
-        Queue<Integer> pq;
-        int k;
+        Queue<Integer> pq = new PriorityQueue<>();
+        int capacity;
 
         public KthLargest(int k, int[] nums) {
-            this.k = k;
+            capacity = k;
             for (int i : nums) {
                 pq.offer(i);
-                if (pq.size() > k) pq.poll();
+                if (pq.size() > capacity) {
+                    pq.poll();
+                }
             }
         }
 
         public int add(int val) {
-            pq.add(val);
-            if (pq.size() > k) pq.poll();
+            pq.offer(val);
+            if (pq.size() > capacity) {
+                pq.poll();
+            }
             return pq.peek();
         }
     }
