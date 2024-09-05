843809e73b90c48ce23d60475af9f9d2d593874c
m1a2st <s7133700@gmail.com>
2024-09-05 08:58:19 +0800

    feat(daily): practice
    2028

---
+++ src/leetcode/medium/No2028.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class No2028 {
+
+    class Solution {
+        public int[] missingRolls(int[] rolls, int mean, int n) {
+            final int targetSum = (rolls.length + n) * mean;
+            int missingSum = targetSum - Arrays.stream(rolls).sum();
+            if (missingSum > n * 6 || missingSum < n)
+                return new int[] {};
+
+            int[] ans = new int[n];
+            Arrays.fill(ans, missingSum / n);
+            missingSum %= n;
+            for (int i = 0; i < missingSum; ++i)
+                ++ans[i];
+
+            return ans;
+        }
+    }
+}
