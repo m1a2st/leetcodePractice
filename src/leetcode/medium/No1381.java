d509aef7202ef136a452fc03aac1c3a8f3b4c341
m1a2st <s7133700@gmail.com>
2024-09-30 08:46:40 +0800

    feat(daily): practice
    1381

---
+++ src/leetcode/medium/No1381.java
@@ -0,0 +1,32 @@
+package leetcode.medium;
+
+public class No1381 {
+
+    class CustomStack {
+        int[] stack;
+        int top;
+
+        public CustomStack(int maxSize) {
+            stack = new int[maxSize];
+            top = -1;
+        }
+
+        public void push(int x) {
+            if (top < stack.length - 1) {
+                stack[++top] = x;
+            }
+        }
+
+        public int pop() {
+            return top > -1 ? stack[top--] : -1;
+        }
+
+        public void increment(int k, int val) {
+            k = Math.min(k, top + 1);
+            for (int i = 0; i < k; i++) {
+                stack[i] += val;
+            }
+        }
+    }
+
+}
