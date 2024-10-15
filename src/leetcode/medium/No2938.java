233aceaa9b7375e94af09da40a0066ed7c92627d
m1a2st <s7133700@gmail.com>
2024-10-16 01:14:40 +0800

    feat(daily): practice
    2938

---
+++ src/leetcode/medium/No2938.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+public class No2938 {
+
+    class Solution {
+        public long minimumSteps(String s) {
+            long res = 0;
+            int i = 0;
+            int j = 0;
+            int n = s.length();
+            while (i < n) {
+                char cur = s.charAt(i);
+                // 遇到白色
+                if (cur == '0') {
+                    res += i - j;
+                    j++;
+                }
+                i++;
+            }
+            return res;
+        }
+    }
+}
