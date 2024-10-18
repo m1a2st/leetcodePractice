d83a9b535bcba1165073bc78b46651e60ef2e535
m1a2st <s7133700@gmail.com>
2024-10-18 08:23:20 +0800

    feat(daily): practice
    2044

---
+++ src/leetcode/medium/No2044.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class No2044 {
+
+
+    class Solution {
+
+        private int ans = 0;
+
+        public int countMaxOrSubsets(int[] nums) {
+            final int ors = Arrays.stream(nums)
+                    .reduce((a, b) -> a | b).getAsInt();
+            dfs(nums, 0, 0, ors);
+            return ans;
+        }
+
+        private void dfs(int[] nums, int i, int path, final int ors) {
+            if (i == nums.length) {
+                if (path == ors)
+                    ++ans;
+                return;
+            }
+
+            dfs(nums, i + 1, path, ors);
+            dfs(nums, i + 1, path | nums[i], ors);
+        }
+    }
+}
