881d3328e5ee0ccb5a72dd74c6e855fc8a38dfde
m1a2st <s7133700@gmail.com>
2024-10-24 08:29:47 +0800

    feat(daily): practice
    951

--- src/leetcode/medium/No951.java
+++ src/leetcode/medium/No951.java
@@ -2,7 +2,7 @@ package leetcode.medium;
 
 public class No951 {
 
-    class Solution {
+    class SolutionOld {
         public boolean flipEquiv(TreeNode root1, TreeNode root2) {
             if (root1 == null)
                 return root2 == null;
@@ -10,6 +10,22 @@ public class No951 {
                 return false;
             if (root1.val != root2.val)
                 return false;
+            return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
+                    flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+        }
+    }
+
+    class Solution {
+        public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+            if (root1 == null) {
+                return root2 == null;
+            }
+            if (root2 == null) {
+                return false;
+            }
+            if (root1.val != root2.val) {
+                return false;
+            }
             return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
                     flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
         }
