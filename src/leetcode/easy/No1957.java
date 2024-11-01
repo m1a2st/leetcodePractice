afd529960aa6dee274c3271e2ecd4076c098118d
m1a2st <s7133700@gmail.com>
2024-11-01 09:10:00 +0800

    feat(daily): practice
    1957

---
+++ src/leetcode/easy/No1957.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+public class No1957 {
+
+    class Solution {
+        public String makeFancyString(String s) {
+            StringBuilder sb = new StringBuilder();
+            int len = s.length();
+
+            for (int i = 0; i < len; i++) {
+                int count = 1;
+                char c = s.charAt(i);
+                sb.append(c);
+                while (i + 1 < len && s.charAt(i + 1) == c) {
+                    count++;
+                    if (count < 3) {
+                        sb.append(c);
+                    }
+                    i++;
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
