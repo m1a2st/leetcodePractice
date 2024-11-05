dabf3e71d9e298b2d6e4e4dd25cb930fce774db8
m1a2st <s7133700@gmail.com>
2024-11-05 08:25:21 +0800

    feat(daily): practice
    2914

---
+++ src/leetcode/medium/No2914.java
@@ -0,0 +1,16 @@
+package leetcode.medium;
+
+public class No2914 {
+
+    class Solution {
+        public int minChanges(String s) {
+            int ans = 0;
+
+            for (int i = 0; i + 1 < s.length(); i += 2)
+                if (s.charAt(i) != s.charAt(i + 1))
+                    ++ans;
+
+            return ans;
+        }
+    }
+}
