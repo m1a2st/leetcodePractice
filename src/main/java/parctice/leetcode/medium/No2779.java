f904925c010ebd3a87eeaf7989db1f7f37bc3db7
m1a2st <s7133700@gmail.com>
2024-12-11 09:27:33 +0800

    feat(daily): practice
    2279

---
+++ src/main/java/parctice/leetcode/medium/No2779.java
@@ -0,0 +1,21 @@
+package parctice.leetcode.medium;
+
+import java.util.Arrays;
+
+public class No2779 {
+
+    class Solution {
+        public int maximumBeauty(int[] nums, int k) {
+            int ans = 0;
+            Arrays.sort(nums);
+
+            for (int l = 0, r = 0; r < nums.length; ++r) {
+                while (nums[r] - nums[l] > 2 * k)
+                    ++l;
+                ans = Math.max(ans, r - l + 1);
+            }
+
+            return ans;
+        }
+    }
+}
