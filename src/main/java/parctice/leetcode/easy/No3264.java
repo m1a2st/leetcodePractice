774e2a795993ab2f55d24b0e97d112d599366670
m1a2st <s7133700@gmail.com>
2024-12-16 09:30:04 +0800

    feat(daily): practice
    3264

---
+++ src/main/java/parctice/leetcode/easy/No3264.java
@@ -0,0 +1,19 @@
+package parctice.leetcode.easy;
+
+public class No3264 {
+
+    class Solution {
+        public int[] getFinalState(int[] nums, int k, int multiplier) {
+            for (int i = 0; i < k; i++) {
+                int minIndex = 0;
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] < nums[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+                nums[minIndex] *= multiplier;
+            }
+            return nums;
+        }
+    }
+}
