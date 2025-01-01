ff76c9908439a9f783da894da6083d8a6f7b0a63
m1a2st <s7133700@gmail.com>
2025-01-01 09:35:48 +0800

    feat(daily): practice
    1422

--- src/main/java/parctice/leetcode/easy/No1422.java
+++ src/main/java/parctice/leetcode/easy/No1422.java
@@ -29,4 +29,32 @@ public class No1422 {
             return ans;
         }
     }
+
+    class Solution2 {
+        public int maxScore(String s) {
+
+            int lSum = s.charAt(0) == '0' ? 1 : 0;
+            int rSum = 0;
+
+            for (int i = 1; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '1') {
+                    rSum++;
+                }
+            }
+            int ans = lSum + rSum;
+
+            for (int left = 1; left < s.length() - 1; left++) {
+                char c = s.charAt(left);
+                if (c == '0') {
+                    lSum++;
+                } else {
+                    rSum--;
+                }
+
+                ans = Math.max(ans, lSum + rSum);
+            }
+            return ans;
+        }
+    }
 }
