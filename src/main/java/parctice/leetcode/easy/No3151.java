ec35a63a78cc2af2ac5107966da7a82151036fe0
m1a2st <s7133700@gmail.com>
2025-02-01 09:34:52 +0800

    feat(daily): practice
    3151

---
+++ src/main/java/parctice/leetcode/easy/No3151.java
@@ -0,0 +1,15 @@
+package parctice.leetcode.easy;
+
+public class No3151 {
+
+    class Solution {
+        public boolean isArraySpecial(int[] nums) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] % 2 == nums[i + 1] % 2) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
