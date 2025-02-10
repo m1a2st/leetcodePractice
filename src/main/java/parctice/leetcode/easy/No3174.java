59e8f3d01ed0501a563414d2f1c2dae48c484a1c
m1a2st <s7133700@gmail.com>
2025-02-10 08:43:12 +0800

    feat(daily): practice
    3174

---
+++ src/main/java/parctice/leetcode/easy/No3174.java
@@ -0,0 +1,25 @@
+package parctice.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+public class No3174 {
+    
+    @Test
+    public void Test() {
+        Solution solution = new Solution();
+        System.out.println(solution.clearDigits("abc"));
+    }
+
+    class Solution {
+        public String clearDigits(String s) {
+            StringBuilder sb = new StringBuilder();
+            for (final char c : s.toCharArray())
+                if (Character.isDigit(c))
+                    sb.setLength(sb.length() - 1);
+                else
+                    sb.append(c);
+
+            return sb.toString();
+        }
+    }
+}
