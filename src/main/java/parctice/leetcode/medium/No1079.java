845bd7a17c728945e2a5c15086297ab031c7ae53
m1a2st <s7133700@gmail.com>
2025-02-17 20:49:24 +0800

    feat(daily): practice
    1079

--- src/main/java/parctice/leetcode/medium/No1079.java
+++ src/main/java/parctice/leetcode/medium/No1079.java
@@ -12,34 +12,7 @@ public class No1079 {
         System.out.println(new Solution().numTilePossibilities("AAB"));
     }
 
-    class Solution {
-
-        Set<String> ans = new HashSet<>();
-        boolean[] visited;
-
-        public int numTilePossibilities(String tiles) {
-            char[] arr = tiles.toCharArray();
-            visited = new boolean[arr.length];
-            backtracking(arr, visited, "");
-            return ans.size() - 1;
-        }
-
-        private void backtracking(char[] arr, boolean[] visited, String sub) {
-            ans.add(sub);
-            for (int i = 0; i < arr.length; i++) {
-                if (visited[i]) {
-                    continue;
-                }
-                visited[i] = true;
-                sub = sub + arr[i];
-                backtracking(arr, visited, sub);
-                sub = sub.substring(0, sub.length() - 1);
-                visited[i] = false;
-            }
-        }
-    }
-
-    class SolutionFast {
+    class Solution{
         public int numTilePossibilities(String tiles) {
             int[] count = new int[26];
             for (char t : tiles.toCharArray())
