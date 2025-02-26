1be191e735611fdc6a1a214fa8b66099affcb9a9
m1a2st <s7133700@gmail.com>
2025-02-26 08:13:18 +0800

    feat(daily): practice
    1749

---
+++ src/main/java/parctice/leetcode/medium/No1749.java
@@ -0,0 +1,20 @@
+package parctice.leetcode.medium;
+
+public class No1749 {
+    
+    class Solution {
+        public int maxAbsoluteSum(int[] nums) {
+            int ans = Integer.MIN_VALUE;
+            int maxSum = 0;
+            int minSum = 0;
+
+            for (final int num : nums) {
+                maxSum = Math.max(num, maxSum + num);
+                minSum = Math.min(num, minSum + num);
+                ans = Math.max(ans, Math.max(maxSum, -minSum));
+            }
+
+            return ans;
+        }
+    }
+}
