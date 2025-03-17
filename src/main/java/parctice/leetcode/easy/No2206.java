14256844d58066ca296bacb96e46c62c881e158e
m1a2st <s7133700@gmail.com>
2025-03-17 08:30:53 +0800

    feat(daily): practice
    2206

---
+++ src/main/java/parctice/leetcode/easy/No2206.java
@@ -0,0 +1,23 @@
+package parctice.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class No2206 {
+
+    class Solution {
+        public boolean divideArray(int[] nums) {
+            int len = nums.length;
+            int numPairs = len / 2;
+            int sum = 0;
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int num : nums){
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            for (Integer value : map.values()) {
+                sum += value / 2;
+            }
+            return sum == numPairs;
+        }
+    }
+}
