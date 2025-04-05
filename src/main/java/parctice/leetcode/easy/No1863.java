0391d7a5739100683f54c71d0754dcd7524e46ce
m1a2st <s7133700@gmail.com>
2025-04-06 00:16:43 +0800

    feat(daily): practice
    1863

--- src/main/java/parctice/leetcode/easy/No1863.java
+++ src/main/java/parctice/leetcode/easy/No1863.java
@@ -1,7 +1,22 @@
 package parctice.leetcode.easy;
 
+import org.junit.jupiter.api.Test;
+
 public class No1863 {
 
+    
+    @Test
+    public void test() {
+        int[] nums = {1, 3};
+        System.out.println(new Solution().subsetXORSum(nums));
+    }
+    
+    @Test
+    public void test2() {
+        int[] nums = {5, 1, 6};
+        System.out.println(new Solution().subsetXORSum(nums));
+    }
+    
     class Solution {
         public int subsetXORSum(int[] nums) {
             return dfs(nums, 0, 0);
