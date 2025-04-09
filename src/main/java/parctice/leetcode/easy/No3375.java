401b9332f326dbf6b5bdebb3269964bd799cb81d
m1a2st <s7133700@gmail.com>
2025-04-10 00:25:32 +0800

    feat(daily): practice
    3375

---
+++ src/main/java/parctice/leetcode/easy/No3375.java
@@ -0,0 +1,16 @@
+package parctice.leetcode.easy;
+
+public class No3375 {
+
+    class Solution {
+        public int minOperations(int[] nums, int k) {
+            Set<Integer> numsSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+            final int mn = Arrays.stream(nums).min().getAsInt();
+            if (mn < k)
+                return -1;
+            if (mn > k)
+                return numsSet.size();
+            return numsSet.size() - 1;
+        }
+    }
+}
