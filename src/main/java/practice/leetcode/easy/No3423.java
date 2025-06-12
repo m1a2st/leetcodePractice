e9ecda13aa41ef1d86caabcb3c9bcebab60200ad
m1a2st <s7133700@gmail.com>
2025-06-12 08:36:40 +0800

    feat(daily): practice
    3423

---
+++ src/main/java/practice/leetcode/easy/No3423.java
@@ -0,0 +1,15 @@
+package practice.leetcode.easy;
+
+public class No3423 {
+
+    class Solution {
+        public int maxAdjacentDistance(int[] nums) {
+            int ans = Math.abs(nums[0] - nums[nums.length - 1]);
+
+            for (int i = 0; i + 1 < nums.length; ++i)
+                ans = Math.max(ans, Math.abs(nums[i] - nums[i + 1]));
+
+            return ans;
+        }
+    }
+}
