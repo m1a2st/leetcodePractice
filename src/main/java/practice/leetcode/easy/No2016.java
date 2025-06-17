eefc9c74a0ec69273f93b920fdf4ba3b236c8d16
m1a2st <s7133700@gmail.com>
2025-06-17 08:17:23 +0800

    feat(daily): practice
    2016

---
+++ src/main/java/practice/leetcode/easy/No2016.java
@@ -0,0 +1,19 @@
+package practice.leetcode.easy;
+
+public class No2016 {
+
+    class Solution {
+        public int maximumDifference(int[] nums) {
+            int ans = -1;
+            int mn = nums[0];
+
+            for (int i = 1; i < nums.length; ++i) {
+                if (nums[i] > mn)
+                    ans = Math.max(ans, nums[i] - mn);
+                mn = Math.min(mn, nums[i]);
+            }
+
+            return ans;
+        }
+    }
+}
