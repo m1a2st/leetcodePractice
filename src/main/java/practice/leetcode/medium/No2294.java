f86b8ffb4e5d8e381e8d57a515d0fcdcee6aa12e
m1a2st <s7133700@gmail.com>
2025-06-19 08:51:26 +0800

    feat(daily): practice
    2294

---
+++ src/main/java/practice/leetcode/medium/No2294.java
@@ -0,0 +1,23 @@
+package practice.leetcode.medium;
+
+import java.util.Arrays;
+
+public class No2294 {
+
+    class Solution {
+        public int partitionArray(int[] nums, int k) {
+            Arrays.sort(nums);
+
+            int ans = 1;
+            int mn = nums[0];
+
+            for (int i = 1; i < nums.length; ++i)
+                if (mn + k < nums[i]) {
+                    ++ans;
+                    mn = nums[i];
+                }
+
+            return ans;
+        }
+    }
+}
