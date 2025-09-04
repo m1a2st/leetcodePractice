d03d14bba7e0d639e95ecbbd160a4570a8f77ad9
m1a2st <s7133700@gmail.com>
2025-09-04 08:47:26 +0800

    feat(daily): practice
    3516

---
+++ src/main/java/practice/leetcode/easy/No3516.java
@@ -0,0 +1,14 @@
+package practice.leetcode.easy;
+
+public class No3516 {
+
+    class Solution {
+        public int findClosest(int x, int y, int z) {
+            final int xz = Math.abs(x - z);
+            final int yz = Math.abs(y - z);
+            if (xz == yz)
+                return 0;
+            return xz < yz ? 1 : 2;
+        }
+    }
+}
