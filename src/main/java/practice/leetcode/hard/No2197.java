5ca34edaaf54033e7622fecb99102fbe71ac3f42
m1a2st <s7133700@gmail.com>
2025-09-16 08:52:06 +0800

    feat(daily): practice
    2197

---
+++ src/main/java/practice/leetcode/hard/No2197.java
@@ -0,0 +1,29 @@
+package practice.leetcode.hard;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class No2197 {
+
+    class Solution {
+        public List<Integer> replaceNonCoprimes(int[] nums) {
+            LinkedList<Integer> ans = new LinkedList<>();
+
+            for (int num : nums) {
+                while (!ans.isEmpty() && gcd(ans.getLast(), num) > 1)
+                    num = lcm(ans.removeLast(), num);
+                ans.addLast(num);
+            }
+
+            return ans;
+        }
+
+        private int gcd(int a, int b) {
+            return b == 0 ? a : gcd(b, a % b);
+        }
+
+        private int lcm(int a, int b) {
+            return a * (b / gcd(a, b));
+        }
+    }
+}
