bd484037dc6b69ee76300ccf6bc3d1f3a52722c7
m1a2st <s7133700@gmail.com>
2025-10-02 08:49:42 +0800

    feat(daily): practice
    3100

---
+++ src/main/java/practice/leetcode/medium/No3100.java
@@ -0,0 +1,17 @@
+package practice.leetcode.medium;
+
+public class No3100 {
+
+    class Solution {
+        public int maxBottlesDrunk(int numBottles, int numExchange) {
+            int ans = numBottles;
+            while (numBottles >= numExchange) {
+                numBottles -= numExchange;
+                ans++;
+                numBottles++;
+                numExchange++;
+            }
+            return ans;
+        }
+    }
+}
