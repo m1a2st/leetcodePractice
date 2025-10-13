75c95e570fecc3840882881ae77ab32ce35d697d
m1a2st <s7133700@gmail.com>
2025-10-13 17:58:38 +0800

    feat(daily): practice
    2273

---
+++ src/main/java/practice/leetcode/easy/No2273.java
@@ -0,0 +1,39 @@
+package practice.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class No2273 {
+
+    class Solution {
+        public List<String> removeAnagrams(String[] words) {
+            List<String> ans = new ArrayList<>();
+
+            for (int i = 0; i < words.length;) {
+                int j = i + 1;
+                while (j < words.length && isAnagram(words[i], words[j]))
+                    ++j;
+                ans.add(words[i]);
+                i = j;
+            }
+
+            return ans;
+        }
+
+        private boolean isAnagram(final String a, final String b) {
+            if (a.length() != b.length())
+                return false;
+
+            int[] count = new int[26];
+
+            for (final char c : a.toCharArray())
+                ++count[c - 'a'];
+
+            for (final char c : b.toCharArray())
+                --count[c - 'a'];
+
+            return Arrays.stream(count).allMatch(c -> c == 0);
+        }
+    }
+}
