edaa134981da266d6d7447b43b2d7af51f0d4276
m1a2st <s7133700@gmail.com>
2025-10-18 20:19:03 +0800

    feat(daily): practice
    3397

---
+++ src/main/java/practice/leetcode/medium/No3397.java
@@ -0,0 +1,23 @@
+package practice.leetcode.medium;
+
+import java.util.Arrays;
+
+public class No3397 {
+
+    class Solution {
+        public int maxDistinctElements(int[] nums, int k) {
+            int ans = 0;
+            int occupied = Integer.MIN_VALUE;
+
+            Arrays.sort(nums);
+
+            for (final int num : nums)
+                if (occupied < num + k) {
+                    occupied = Math.max(occupied + 1, num - k);
+                    ++ans;
+                }
+
+            return ans;
+        }
+    }
+}
