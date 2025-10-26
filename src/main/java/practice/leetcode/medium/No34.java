48aed2307175e70353d086dd3031059b3a8b3121
m1a2st <s7133700@gmail.com>
2025-10-26 15:00:34 +0800

    feat(daily): practice
    34

--- src/main/java/practice/leetcode/medium/No34.java
+++ src/main/java/practice/leetcode/medium/No34.java
@@ -4,29 +4,25 @@ public class No34 {
 
     class Solution {
         public int[] searchRange(int[] nums, int target) {
-            int[] ans = {-1, -1};
-            int len = nums.length;
-            int left = 0, right = len;
+            int left = findLeast(nums, target);
+            if (left == nums.length || nums[left] != target) {
+                return new int[]{-1, -1};
+            }
+            int right = findLeast(nums, target + 1) - 1;
+            return new int[]{left, right};
+        }
+
+        private int findLeast(int[] nums, int target) {
+            int left = 0, right = nums.length;
             while (left < right) {
                 int mid = (right - left) / 2 + left;
-                if (nums[mid] == target) {
-                    int start = mid, end = mid;
-                    while (start >= 0 && nums[start] == nums[mid]) {
-                        start--;
-                    }
-                    while (end < len && nums[end] == nums[mid]) {
-                        end++;
-                    }
-                    ans[0] = start + 1;
-                    ans[1] = end - 1;
-                    break;
-                } else if (nums[mid] > target) {
+                if (nums[mid] >= target) {
                     right = mid;
                 } else {
                     left = mid + 1;
                 }
             }
-            return ans;
+            return left;
         }
     }
 }
