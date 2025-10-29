4321a484d95509c09f95c5e34dc4396df178cb4f
m1a2st <s7133700@gmail.com>
2025-10-29 20:47:37 +0800

    feat(daily): practice
    3370

---
+++ src/main/java/practice/leetcode/easy/No3370.java
@@ -0,0 +1,14 @@
+package practice.leetcode.easy;
+
+public class No3370 {
+
+    class Solution {
+        public int smallestNumber(int n) {
+            return (1 << bitLength(n)) - 1;
+        }
+
+        private int bitLength(int n) {
+            return Integer.SIZE - Integer.numberOfLeadingZeros(n);
+        }
+    }
+}
