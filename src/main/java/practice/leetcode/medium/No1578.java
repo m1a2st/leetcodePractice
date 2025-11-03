479f178b18a7c0934e5e2afcba910216fdb75b48
m1a2st <s7133700@gmail.com>
2025-11-03 08:31:19 +0800

    feat(daily): practice
    1578

--- src/main/java/practice/leetcode/medium/No1578.java
+++ src/main/java/practice/leetcode/medium/No1578.java
@@ -19,20 +19,4 @@ public class No1578 {
             return ans;
         }
     }
-
-    class SolutionNew {
-        public int minCost(String colors, int[] neededTime) {
-            int ans = 0;
-            int maxNeedTime = neededTime[0];
-            for (int i = 1; i < colors.length(); i++) {
-                if (colors.charAt(i) == colors.charAt(i - 1)) {
-                    ans += Math.min(maxNeedTime, neededTime[i]);
-                    maxNeedTime = Math.max(maxNeedTime, neededTime[i]);
-                } else {
-                    maxNeedTime = neededTime[i];
-                }
-            }
-            return ans;
-        }
-    }
 }
