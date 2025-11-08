e4f2793d4c980ea3d9a77ec4fa3741fe4c8a3d76
m1a2st <s7133700@gmail.com>
2025-11-08 22:34:02 +0800

    feat(daily): practice
    221

---
+++ src/main/java/practice/leetcode/medium/No221.java
@@ -0,0 +1,38 @@
+package practice.leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+public class No221 {
+
+    @Test
+    void testMaximalSquare() {
+        Solution solution = new Solution();
+        char[][] matrix = {
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '1', '1', '1'}
+        };
+        solution.maximalSquare(matrix);
+    }
+
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            final int m = matrix.length;
+            final int n = matrix[0].length;
+            int[][] dp = new int[m][n];
+            int maxLength = 0;
+
+            for (int i = 0; i < m; ++i)
+                for (int j = 0; j < n; ++j) {
+                    if (i == 0 || j == 0 || matrix[i][j] == '0')
+                        dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
+                    else
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                }
+
+            return maxLength * maxLength;
+        }
+    }
+}
